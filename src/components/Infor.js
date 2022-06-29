9d273becaf983f70a7967581079c6e2353c550cc
RefMags <fyfymags@gmail.com>
2022-06-29 20:00:03 +0900

    testingcommite

--- src/components/Infor.js
+++ src/components/Infor.js
@@ -7,6 +7,10 @@ export default function Infor() {
     <div>
       <img className="profile-pic"  src={RefPhoto} alt=""  width="45px"/>
       <h2>Ref Mags</h2>
+      <h2>Ref Mags</h2>
+      <h2>Ref Mags</h2>
+      <h2>Ref Mags</h2>
+      <h2>Ref Mags</h2>
       <h3>Fullstack developer</h3>
       <h3>Fullstack developer</h3>
       <p>email</p>
